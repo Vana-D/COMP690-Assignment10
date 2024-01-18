7c83690c587a338f138ff80c83103353899c185a
Vana-D <vana.y.do11@gmail.com>
2024-01-17 17:20:39 -0800

    updated btns

--- js/player.js
+++ js/player.js
@@ -7,7 +7,13 @@
 // HELPER FUNCTION TO RETURN DOM ELEMENTS
 const $ = (id) => { return document.getElementById(id)};
 
-let fav = document.createElement('p');
+
+let selectValue = albumNames;
+let index0 = 0;
+let index1 = 0;
+let index2 = 0;
+
+
 class Jukebox {
     constructor(albums) {
         this._albums = []
@@ -41,21 +47,53 @@ class Album {
     constructor(artist, title) {
         this.artist = artist
         this.title = title
-        this.played = 0
+        this.played = 0;
     }
+
     play = function() {
-        this.played += 1
-        // ADD EVENT LISTENER TO PLAY BUTTON
-        playBtn.addEventListener('click', () => {
-            albumListValue = albumList.value;
-            fav.innerHTML = `${jbox.favoriteAlbum()}`;
-        });
+        this.played = this.played +1;
+        this.played++;
+        // DISPLAY FAVORITE BUTTON
+        if (this.played = 0) {
+            favBtn.classList.add('d-none');
+        } else {
+            favBtn.classList.remove('d-none');
+        };
+        
     }
+    
     display = function() {
         return `${this.artist} : ${this.title}. The album has been played ${this.played} times.`
-
-    }
+        
+    }   
+    
 }
+
+const played = playBtn.addEventListener('click', () => {
+    // for each select increase count
+    if ((selectValue).value == '2') { 
+        index0 = index0 + 1;
+        album3.play()
+        console.log(`Album3 - New Found Glory played ${index0} times.`)
+    } else if((selectValue).value == '1') { 
+        index1 = index1 + 1;
+        album2.play()
+        console.log(`Album2 - Blink182 played ${index1} times.`)
+    } else if ((selectValue).value == '0')  { 
+        index2 = index2 + 1;
+        album1.play()
+        console.log(`Album1 - Operation Ivy played ${index2} times.`)
+    } else {
+        console.log('Please make a selection.')
+    }
+   
+    favBtn.addEventListener('click', () => {
+        jbox.favoriteAlbum();
+        favoriteAlbum.innerHTML = `${jbox.favoriteAlbum()}`;
+        console.log(`Your favorite album is: ${jbox.favoriteAlbum()}`)
+    })
+        
+});  
 
 let jbox = new Jukebox()
 const album1 = new Album('Operation Ivy', 'Energy');
@@ -67,7 +105,12 @@ jbox.addAlbum(album2)
 jbox.addAlbum(album3)
 jbox.addOptions()
 
-// album1.play()
+
+
+
+
+
+
 // album2.play()
 // album2.play()
 // album2.play()
@@ -75,18 +118,10 @@ jbox.addOptions()
 // album2.play()
 // album3.play()
 
-console.log(`Your favorite album is: ${jbox.favoriteAlbum()}`)
 
 
-// favoriteAlbum.appendChild(fav);
-// fav.innerHTML = `${jbox.favoriteAlbum()}`;
+
+
 
 
 //  ---------------------------------------------
-
-
-
-
-
-
-
